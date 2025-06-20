7d9c22ff7fd2b46c27f3e1e83f0ffc26960571b1
cloudops210 <realseniordev210@gmail.com>
2025-06-20 17:54:47 +0800

    update ui

--- src/app.ts
+++ src/app.ts
@@ -16,12 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(cors());
 app.use(helmet());
 app.use(morgan('dev'));
 
